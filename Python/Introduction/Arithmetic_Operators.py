4f142da714f70a731f2d9662f175e15cad7d79b3
Aniruddh Upadhyay <aniruddhupadhyay07@gmail.com>
2021-04-13 18:10:05 +0530

    Create Arithmetic_Operators.py

---
+++ Python/Introduction/Arithmetic_Operators.py
@@ -0,0 +1,31 @@
+# Arithmetic Operators
+
+# Task: 
+# Read two integers from STDIN and print three lines where:
+# The first line contains the sum of the two numbers.
+# The second line contains the difference of the two numbers (first - second).
+# The third line contains the product of the two numbers.
+
+# Input Format: 
+# The first line contains the first integer, a. The second line contains the second integer, b.
+
+# Output Format: 
+# Print the three lines as explained above.
+
+# Sample Input: 
+# 3
+# 2
+
+# Sample Output: 
+# 5
+# 1
+# 6
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT.
+a = int(raw_input())
+b = int(raw_input())
+
+print a + b
+print a - b
+print a * b
+
