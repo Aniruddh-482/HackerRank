90a000863bb9e56f6847e0a1bab89464924feb4c
Aniruddh Upadhyay <aniruddhupadhyay07@gmail.com>
2021-04-13 18:21:29 +0530

    Create Python_Division.py

---
+++ Python/Introduction/Python_Division.py
@@ -0,0 +1,33 @@
+# Python: Division
+
+# Task: 
+# The provided code stub reads two integers, 'a' and 'b', from STDIN.
+# Add logic to print two lines. The first line should contain the result of integer division, a // b.
+# The second line should contain the result of float division, a / b.
+
+# Example:
+# a = 3
+# b = 5
+# The result of the integer division 3 // 5 = 0.
+# The result of the float division is 3 / 5 = 0.6.
+# Print:
+# 0
+# 0.6
+
+# Input Format: 
+# The first line contains the first integer, 'a'.
+# The second line contains the second integer, 'b'.
+
+# Output Format: 
+# Print the two lines as described above.
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT.
+a = int(raw_input())
+b = int(raw_input())
+
+print a // b
+print a / b
+# OR
+print a / b
+print float(a) / b
+
