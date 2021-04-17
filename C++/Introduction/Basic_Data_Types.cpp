8c1adeaa72a32d9ca8743d71a28c2859c3b76216
Aniruddh Upadhyay <aniruddhupadhyay07@gmail.com>
2021-04-17 20:19:29 +0530

    Create Basic_Data_Types.cpp

---
+++ C++/Introduction/Basic_Data_Types.cpp
@@ -0,0 +1,95 @@
+  
+/* 
+Basic Data Types
+
+* Some C++ data types, their format specifiers, and their most common bit widths are as follows: 
+  >  Int ("%d"): 32 Bit integer
+  >  Long ("%ld"): 64 bit integer
+  >  Char ("%c"): Character type
+  >  Float ("%f"): 32 bit real value
+  >  Double ("%lf"): 64 bit real value
+
+Reading: 
+To read a data type, use the following syntax:
+  scanf("`format_specifier`", &val)
+
+For example, to read a character followed by a double:
+  char ch;
+  double d;
+  scanf("%c %lf", &ch, &d);
+
+For the moment, we can ignore the spacing between format specifiers.
+
+Printing: 
+To print a data type, use the following syntax:
+  printf("`format_specifier`", val)
+
+For example, to print a character followed by a double:
+  char ch = 'd';
+  double d = 234.432;
+  printf("%c %lf", ch, d);
+
+Note: You can also use cin and cout instead of scanf and printf; however, 
+if you are taking a million numbers as input and printing a million lines, 
+it is faster to use scanf and printf.
+
+Input Format: 
+Input consists of the following space-separated values: int, long, char, float, and double, respectively.
+
+Output Format: 
+Print each element on a new line in the same order it was received as input. 
+Note that the floating point value should be correct up to 3 decimal places and the double to 9 decimal places.
+*/
+
+
+#include <iostream>
+#include <cstdio>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    // Complete the code.
+    int a;
+    long b;
+    char c;
+    float d;
+    double e;
+    
+    cin>> a >> b >> c >> d >> e;
+    
+    cout<< a << endl<< b << endl << c << endl;
+    
+    cout << fixed << setprecision(3) << d << endl;
+   
+    cout << fixed << setprecision(9) << e << endl;
+    
+    return 0;
+}
+
+// OR
+
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    // Complete the code.
+    int a;
+    long l; 
+    char c;
+    float  f; 
+    double d;
+    
+    cin>>a>>l>>c>>f>>d;
+    
+    cout<<a<<endl<<l<<endl<<c<<endl;
+    
+    cout.precision(3);
+    cout<<fixed<<f<<endl;
+    
+    cout.precision(9);
+    cout<<fixed<<d;
+    
+    return 0;
+}
+
